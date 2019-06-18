d193fa1de2e81d498e7c6e7f85de658975aee883
Twan Koolen <koolen.twan@gmail.com>
2019-06-18 14:32:18 -0400

    Add constraint violation tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,10 @@ using JuMP
 using LinearAlgebra
 using Test
 
+using Gurobi
+
 include("isconvex.jl")
+include("constraint_violation.jl")
 include("adhya1.jl")
 # include("pooling_infra.jl") # requires unavailable csv file
 include("pooling.jl")
